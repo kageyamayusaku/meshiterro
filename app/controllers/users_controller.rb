61f5496654fd8140b71bf6fda485fb665bc44900
EC2 Default User <ec2-user@ip-172-31-2-145.ap-northeast-1.compute.internal>
2022-12-20 12:03:07 +0000

    [Finish]15章

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,18 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = ユーザーの取得
+    @user.update
+    redirect_to ユーザーの詳細ページへのパス  
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
   end
 end
