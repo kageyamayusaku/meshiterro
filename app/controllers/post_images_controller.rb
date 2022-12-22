44e91d070929803900eb73e1e0594288eeda4a85
EC2 Default User <ec2-user@ip-172-31-2-145.ap-northeast-1.compute.internal>
2022-12-22 06:07:47 +0000

    [Finish]19章

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -2,7 +2,7 @@ class PostImagesController < ApplicationController
   def new
     @post_image = PostImage.new
   end
-  
+
    def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
@@ -18,14 +18,14 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.find(params[:id])
     @post_comment = PostComment.new
   end
-  
+
   def destroy
     @post_image = PostImage.find(params[:id])
     @post_image.destroy
     redirect_to post_images_path
   end
-  
-  
+
+
   private
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
